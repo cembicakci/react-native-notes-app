dc39b946e86fc7a9efcca02ada6ba030f620e2ac
Cem Bıçakcı <cmbicakci@gmail.com>
2023-06-25 23:13:54 +0300

    installed asyncStorage

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const [text, setText] = useState<string>('')
+
+  const saveNote = async () => {
+    await AsyncStorage.setItem('note', text)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        multiline={true}
+        style={styles.textInput}
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <Button title='Save note' onPress={() => { saveNote() }} />
     </View>
   );
 }
@@ -17,4 +33,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    backgroundColor: '#ffb70342',
+    width: '100%',
+    height: 200,
+    fontSize: 16,
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    paddingBottom: 20
+  }
 });
